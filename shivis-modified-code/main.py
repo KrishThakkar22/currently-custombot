2e1f3fb1727a06c39615f6dcfe35a59b76abf918
KrishThakkar22 <krish.t@hashtechy.com>
2025-08-27 18:51:41 +0530

    Chaging PORT from 8000 to 3000

--- shivis-modified-code/main.py
+++ shivis-modified-code/main.py
@@ -50,4 +50,4 @@ async def startup_event():
     asyncio.create_task(cleanup_replied_ids())
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=3000, reload=True)
