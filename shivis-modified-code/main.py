b5a215a4483db273f7c696e003e12ee29b09047d
KrishThakkar22 <krish.t@hashtechy.com>
2025-08-27 18:53:14 +0530

    Chaging IP to 127.0.0.1

--- shivis-modified-code/main.py
+++ shivis-modified-code/main.py
@@ -50,4 +50,4 @@ async def startup_event():
     asyncio.create_task(cleanup_replied_ids())
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=3000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=3000, reload=True)
